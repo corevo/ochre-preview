5a6d3db1722116ff458cfcc318570277a4a6f97f
Tomer <gmtomer@gmail.com>
2016-06-29 17:26:49 +0300

    fixed an issue with character escaping

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec;
 
 function convertToPDF(path, out, cb) {
-    exec('libreoffice5.0 --headless --convert-to pdf:writer_pdf_Export --outdir="' + out + '" "' + path + '"', function (error, stdout, stderr) {
+    exec('libreoffice5.0 --headless --convert-to pdf:writer_pdf_Export --outdir="' + out.replace(/(.)/g, "\\$1") + '" "' + path.replace(/(.)/g, "\\$1") + '"', function (error, stdout, stderr) {
         cb(error, stdout);
     });
 }
